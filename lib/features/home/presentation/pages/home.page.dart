86315ec4007d9430e7f66b5a4b00d5a8d2229c51
Maxime Lemesle <maxime.lemesle7@gmail.com>
2025-03-05 23:17:43 +0100

    feat(home): Fetch the user name in the home page

--- lib/features/home/presentation/pages/home.page.dart
+++ lib/features/home/presentation/pages/home.page.dart
@@ -1,48 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:memora_app/features/album/presentation/widgets/album_list.widget.dart';
 import 'package:memora_app/core/widgets/main_app_bar.widget.dart';
 import 'package:memora_app/core/widgets/main_nav_bar.widget.dart';
+import 'package:memora_app/features/user/presentation/blocs/user.bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      context.read<UserBloc>().fetchUser(currentUser.uid);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MainAppBar(
-        title: 'Bonjour,',
-        // TODO - Get user name from user bloc
-        name: 'Maxime',
-      ),
-      body: SizedBox.expand(
-        child: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 24, top: 24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                spacing: 6,
-                children: [
-                  Text(
-                    'Mes albums',
-                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface,
-                        ),
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        String userName = "Invité";
+
+        if (state is UserLoaded) {
+          userName = state.user.name;
+        } else if (state is UserLoading) {
+          userName = "Chargement...";
+        } else if (state is UserError) {
+          userName = "Erreur utilisateur";
+        }
+
+        return Scaffold(
+          appBar: MainAppBar(
+            title: 'Bonjour,',
+            name: userName,
+          ),
+          body: SizedBox.expand(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 24, top: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 6,
+                    children: [
+                      Text(
+                        'Mes albums',
+                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                      ),
+                      AlbumList(),
+                    ],
                   ),
-                  AlbumList(),
-                ],
-              ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: MainNavBar(),
+                ),
+              ],
             ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: MainNavBar(),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:memora_app/config/theme/app_theme.dart';
 import 'package:memora_app/features/home/presentation/pages/home.page.dart';
 import 'package:memora_app/features/landing/presentation/pages/landing.page.dart';
+import 'package:memora_app/features/user/data/data-sources/user.data_source.dart';
+import 'package:memora_app/features/user/data/repositories/user.repository_impl.dart';
+import 'package:memora_app/features/user/domain/usecases/get_user.usecase.dart';
+import 'package:memora_app/features/user/presentation/blocs/user.bloc.dart';
 import 'package:memora_app/firebase_options.dart';
 import 'package:memora_app/features/auth/presentation/pages/login.page.dart';
 import 'package:memora_app/features/auth/presentation/pages/signup.page.dart';
@@ -12,7 +18,19 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  final userRepository = UserRepositoryImpl(
+    dataSource: UserDataSource(
+      firestore: FirebaseFirestore.instance,
+    ),
+  );
+
+  runApp(
+    BlocProvider(
+      create: (context) => UserBloc(GetUser(userRepository)),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
