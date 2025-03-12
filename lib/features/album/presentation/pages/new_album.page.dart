9bff5eb0c66d5b16ec320ec73a833ecae46ccfad
Maxime Lemesle <maxime.lemesle7@gmail.com>
2025-03-13 00:25:56 +0100

    wip(NewAlbumPage): Work on the CupertinoDatePicker

--- lib/features/album/presentation/pages/new_album.page.dart
+++ lib/features/album/presentation/pages/new_album.page.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memora_app/core/widgets/main_app_bar.widget.dart';
 
-class NewAlbumPage extends StatelessWidget {
+class NewAlbumPage extends StatefulWidget {
   const NewAlbumPage({super.key});
+
+  @override
+  State<NewAlbumPage> createState() => _NewAlbumPageState();
+}
+
+class _NewAlbumPageState extends State<NewAlbumPage> {
+  DateTime dateTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +23,7 @@ class NewAlbumPage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(24.0),
                 child: Column(
+                  spacing: 24,
                   children: [
                     /// SELECT BACKGROUND PHOTO
                     Container(
@@ -30,7 +39,7 @@ class NewAlbumPage extends StatelessWidget {
                       ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        spacing: 8,
+                        spacing: 12,
                         children: [
                           Container(
                             padding: EdgeInsets.all(8),
@@ -52,6 +61,108 @@ class NewAlbumPage extends StatelessWidget {
                             'Ajouter une photo de couverture',
                             style: Theme.of(context).textTheme.titleSmall,
                             textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    /// SELECT TITLE OF THE ALBUM
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.surfaceContainer,
+                        border: Border.all(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.outline,
+                        ),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: TextField(
+                              style: Theme.of(context).textTheme.titleSmall,
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Ajouter une titre à mon album',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    /// SELECT DATE OF THE ALBUM
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.surfaceContainer,
+                        border: Border.all(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.outline,
+                        ),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Row(
+                              children: [
+                                CupertinoButton(
+                                  child: Text(
+                                      '${dateTime.day}/${dateTime.month}/${dateTime.year}'),
+                                  onPressed: () {
+                                    showModalBottomSheet(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 24, left: 24, right: 24),
+                                              child: Text(
+                                                'Sélectionner la date de début de l\'album',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .titleSmall,
+                                                textAlign: TextAlign.center,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 350,
+                                              width: double.infinity,
+                                              child: CupertinoDatePicker(
+                                                initialDateTime: dateTime,
+                                                backgroundColor:
+                                                    Theme.of(context)
+                                                        .colorScheme
+                                                        .surface,
+                                                onDateTimeChanged:
+                                                    (DateTime newDateTime) {
+                                                  setState(() {
+                                                    dateTime = newDateTime;
+                                                  });
+                                                },
+                                                mode: CupertinoDatePickerMode
+                                                    .date,
+                                                dateOrder:
+                                                    DatePickerDateOrder.ymd,
+                                              ),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
