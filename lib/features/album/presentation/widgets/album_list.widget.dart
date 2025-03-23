0e5a021953b5c0ea1e8cd51acd73a9974a6a732d
Maxime Lemesle <maxime.lemesle7@gmail.com>
2025-03-23 10:39:14 +0100

    fix(homepage): Add a scrollsnap to the list of album

--- lib/features/album/presentation/widgets/album_list.widget.dart
+++ lib/features/album/presentation/widgets/album_list.widget.dart
@@ -15,90 +15,99 @@ class AlbumList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 644,
       width: MediaQuery.of(context).size.width,
-      child: ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.only(left: 24),
+      child: PageView.builder(
+        controller: PageController(
+          viewportFraction: 0.85,
+          initialPage: 0,
+        ),
+        padEnds: false,
         itemCount: albums.length + 1,
-        separatorBuilder: (context, index) => const SizedBox(width: 24),
         itemBuilder: (context, index) {
           if (index < albums.length) {
             final album = albums[index];
 
             // Fetch the user with the ownerId
-            return StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(album.ownerId)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                final userName =
-                    snapshot.data?.get('name') ?? 'Utilisateur inconnu';
+            return Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: StreamBuilder<DocumentSnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(album.ownerId)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  final userName =
+                      snapshot.data?.get('name') ?? 'Utilisateur inconnu';
 
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 24,
-                  children: [
-                    CoverPage(
-                      title: album.title,
-                      backgroundImage: album.backgroundImage,
-                      startDate: album.startDate,
-                      endDate: album.endDate,
-                      members: album.members ?? [],
-                    ),
-                    AlbumDescription(
-                      owner: userName,
-                      description: album.description ?? '',
-                    ),
-                  ],
-                );
-              },
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 24,
+                    children: [
+                      CoverPage(
+                        title: album.title,
+                        backgroundImage: album.backgroundImage,
+                        startDate: album.startDate,
+                        endDate: album.endDate,
+                        members: album.members ?? [],
+                      ),
+                      AlbumDescription(
+                        owner: userName,
+                        description: album.description ?? '',
+                      ),
+                    ],
+                  );
+                },
+              ),
             );
           } else {
             // Add the button to create a new album
-            return Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                height: 450,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  border: Border.all(
-                    color: Theme.of(context).colorScheme.outline,
+            return Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  height: 450,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    color: Colors.transparent,
                   ),
-                  color: Colors.transparent,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 16,
-                  children: [
-                    ButtonWidget(
-                      label: '',
-                      variant: ButtonVariant.tertiary,
-                      size: ButtonSize.big,
-                      iconPosition: ButtonIcon.only,
-                      icon: CupertinoIcons.add,
-                      onPressed: () async {
-                        final albumBloc = context.read<AlbumBloc>();
-                        final currentUser = FirebaseAuth.instance.currentUser;
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 16,
+                    children: [
+                      ButtonWidget(
+                        label: '',
+                        variant: ButtonVariant.tertiary,
+                        size: ButtonSize.big,
+                        iconPosition: ButtonIcon.only,
+                        icon: CupertinoIcons.add,
+                        onPressed: () async {
+                          final albumBloc = context.read<AlbumBloc>();
+                          final currentUser = FirebaseAuth.instance.currentUser;
 
-                        final result = await Navigator.of(context)
-                            .pushNamed('/new_album_page');
+                          final result = await Navigator.of(context)
+                              .pushNamed('/new_album_page');
 
-                        if (result == true && currentUser != null) {
-                          albumBloc.fetchAlbums(currentUser.uid);
-                        }
-                      },
-                    ),
-                    Text(
-                      "Créer un nouvel album",
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                  ],
+                          if (result == true && currentUser != null) {
+                            albumBloc.fetchAlbums(currentUser.uid);
+                          }
+                        },
+                      ),
+                      Text(
+                        "Créer un nouvel album",
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
